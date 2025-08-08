b09ce17e392a350356456c10a4a025bc710bc201
jemaxmars <jemaxmars@icloud.com>
2025-08-08 17:36:58 -0400

    Fix webpack config to point to src/index.html template
    - Updated HtmlWebpackPlugin template path from './index.html' to './src/index.html'
    - Build now works correctly with merged codebase
    - Resolved template path issue after removing root index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "./src/index.html",
       filename: "index.html",
     }),
   ],
