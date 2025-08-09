e5ea7bbd43da7b4d94016fa9c24ca157a85a13d4
akshayk423 <akshay.khalasi3@gmail.com>
2025-08-09 12:44:05 -0700

    created dropdown and refactored css files to match BEM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import { App } from "./components/App/App.js";
+import "./index.css";
 
 console.log("Campus Starter App loaded successfully!");
 
-
 document.addEventListener("DOMContentLoaded", function () {
   new App();
+});
+
+//faq dropdown logic
+document.querySelectorAll(".faq__item").forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("show");
+  });
 });
